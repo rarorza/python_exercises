8a7c8c48192ab1ae281c2cf9a395c337b9fe9370
Rafael <rarorza@proton.me>
2023-07-01 13:22:18 -0300

    Translating the exercise 020 into English

--- curso_em_video/08_utilizando_modulos/020_sorteando_uma_ordem_na_lista.py
+++
@@ -1,7 +0,0 @@
-# Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada
-
-import random
-
-alunos = ["Maria", "João", "Gustavo", "Rafael"]
-random.shuffle(alunos)
-print(alunos)
