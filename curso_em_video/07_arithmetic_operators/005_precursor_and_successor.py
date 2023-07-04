4532efe7f0277238bd07ffd4fff5994cf20b8608
Rafael <rarorza@proton.me>
2023-07-03 21:59:25 -0300

    Update

--- curso_em_video/07_arithmetic_operators/005_precursor_and_successor.py
+++
@@ -1,8 +0,0 @@
-# Make a program to read an integer and print its successor and precursor:
-
-number = int(input("Type a number: "))
-
-print(
-f"""Parsing the value {number}:
-Your precursor is {number - 1} and your successor is {number + 1}"""
-)
--- curso_em_video/08_using_modules/020_randomly_sorting_list.py
+++
@@ -1,8 +0,0 @@
-# Develop a program to read a name in between four students and display them in
-# a random order.
-
-import random
-
-students = ["Maria", "João", "Gustavo", "Rafael"]
-random.shuffle(students)
-print(students)
