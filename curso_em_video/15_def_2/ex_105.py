71c561cc470ac0902b86e89ac3843f80992cf6c3
Rafael Rosa <rarorza@proton.me>
2023-08-07 14:44:37 -0300

    Resolution of exercise 105

---
+++ curso_em_video/15_def_2/ex_105.py
@@ -0,0 +1,22 @@
+# Develop a mini-system that uses Python's interactive help. The user will type
+# the command and the manual will appear. When the user types "END", the program
+# will exit.
+
+def pyHelper():
+    while True:
+        print("~" * 30)
+        print("HELP SYSTEM PyHelper".center(30))
+        print("~" * 30)
+        print("function or library > ", end="")
+        command = input()
+        if command.upper().strip() == "END":
+            print("~" * 30)
+            print("See you later!".center(15))
+            print("~" * 30)
+            break
+        else:
+            print(f"Accessing the {command} command manual")
+            help(command)
+
+
+pyHelper()
