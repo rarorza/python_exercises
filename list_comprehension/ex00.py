7bc7a33b402ff8336a2b8ce8bb2e4d6b2b319ced
Rafael Rosa <rarorza@proton.me>
2023-08-17 19:51:30 -0300

    Exercises to list comprehension

---
+++ list_comprehension/ex00.py
@@ -0,0 +1,11 @@
+# Given a list of numbers, remove all odd numbers from the list:
+
+numbers = [3,5,45,97,32,22,10,19,39,43]
+
+result = [number for number in numbers if number % 2 == 0]
+
+print(result)
+
+# Explaining syntax
+#
+# [output_expression() for(set of values to iterate) if(conditional filtering)]
