2fc0c2ff03ad7207231cf9ce94b925ad18e30b6d
Rafael Rosa <rarorza@proton.me>
2023-08-18 14:24:56 -0300

    Resolution of exercise 06

---
+++ list_comprehension/ex06.py
@@ -0,0 +1,8 @@
+# Get the index and the value as a tuple for items in the list:
+# ["hi", 4, 8.99, 'apple', ('t,b','n')].
+# Result would look like [(index, value), (index, value)]
+
+values = ["hi", 4, 8.99, 'apple', ('t,b','n')]
+result = [(i, value) for i, value in enumerate(values) if value]
+
+print(result)
