09a94725e866f854b267d8b802c6bfc2ce45e786
Rafael Rosa <rarorza@proton.me>
2023-08-18 14:28:39 -0300

    Format code

--- list_comprehension/ex00.py
+++ list_comprehension/ex00.py
@@ -1,6 +1,6 @@
 # Given a list of numbers, remove all odd numbers from the list:
 
-numbers = [3,5,45,97,32,22,10,19,39,43]
+numbers = [3, 5, 45, 97, 32, 22, 10, 19, 39, 43]
 
 result = [number for number in numbers if number % 2 == 0]
 
