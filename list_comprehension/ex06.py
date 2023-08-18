42115124b29d775d77a12229d2beded3c3e852a9
Rafael Rosa <rarorza@proton.me>
2023-08-18 14:26:48 -0300

    Resolution of exercise 06

--- list_comprehension/ex06.py
+++ list_comprehension/ex06.py
@@ -2,7 +2,7 @@
 # ["hi", 4, 8.99, 'apple', ('t,b','n')].
 # Result would look like [(index, value), (index, value)]
 
-values = ["hi", 4, 8.99, 'apple', ('t,b','n')]
+values = ["hi", 4, 8.99, "apple", ("t,b", "n")]
 result = [(i, value) for i, value in enumerate(values) if value]
 
 print(result)
