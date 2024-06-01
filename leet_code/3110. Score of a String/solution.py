354403a91ef67918b233a1bfc6d3f6f64fb152fb
Rafael R Rosa <rarorza@proton.me>
2024-06-01 14:39:26 -0300

    feat: leetcode - 3110 - solution

---
+++ leet_code/3110. Score of a String/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def scoreOfString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        chars = list(s)
+        total = 0
+        for i, char in enumerate(chars):
+            if i + 1 <= len(chars) - 1:
+                total += (abs (ord(char) - ord(chars[i + 1])))
+        return total
+
+if __name__ == '__main__':
+    start = Solution().scoreOfString('hello')
+    print(start)
